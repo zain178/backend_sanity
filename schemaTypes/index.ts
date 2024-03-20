a701c1ddfed34faee7ffbdd70b174e79775fa7fc
Zain Naqvi <zainnaqvi0@hotmail.com>
2024-03-20 21:14:37 +1100

    backend commit

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,1 +1,10 @@
-export const schemaTypes = []
+export const schemaTypes = [testimonials, abouts, brands
+, contact, experiences, skills, workExperience, works];
+import testimonials from './testimonials';
+import abouts from './abouts';
+import brands from './brands';
+import contact from './contact';
+import experiences from './experiences';
+import skills from './skills';
+import workExperience from './workExperience';
+import works from './works';
